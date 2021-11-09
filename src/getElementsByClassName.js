542b6252c4b236cbe7214284935085154fef6b6c
sleepisgood1 <jae1994krw@gmail.com>
2021-11-08 20:51:09 -0800

    Assignment Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // declare a var for array
+  var sameClass = [];
+  var element = document.body;
+  //create a function to recurse over that will search element for the className
+  var searchForClassName = function(element) {
+  // check each element for the className and add to the var
+    if (element.classList && element.classList.contains(className)) {
+      sameClass.push(element);
+    }
+    // check each element for a childnode
+    if (element.hasChildNodes()) {
+      //iterate over the childnodes
+      for (var i = 0; i < element.childNodes.length; i++) {
+        //check if each childnode has clasName by recursing over the function
+        searchForClassName(element.childNodes[i]);
+      }
+    }
+  };
+  searchForClassName(element);
+  return sameClass;
+  //  if there is a childnode
+  //  do a recursive function call
+  // return array with all of the nodes with the mathcing className
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,63 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arrayValues = [];
+  var objectValues = [];
+  //undefined, function
+  if (!obj && typeof obj !== 'boolean' && typeof obj !== 'number') {
+    return 'null';
+  }
+  //number, boolean,
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    //loop over the array
+    for (let i = 0; i < obj.length; i++) {
+      arrayValues.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arrayValues + ']';
+  } else if (obj instanceof Object) {
+    for (var keyForObj in obj) {
+      if (typeof obj[keyForObj] === 'function' || obj[keyForObj] === undefined) {
+      } else {
+        var pair = '"' + keyForObj + '":' + stringifyJSON(obj[keyForObj]);
+        objectValues.push(pair);
+      }
+    }
+    return '{' + objectValues + '}';
+  }
+  //string
+  //declare an empty string
+  // var resultString = '';
+  // //iterate through obj
+  // var resultArrayForArray = [];
+  // var resultArrayForObj = [];
+  // //check what typeof thing it is
+  // if (obj instanceof Object) {
+  //   var keyForObj = Object.keys(obj);
+  //   resultstring += '{' + keyforobj + ':';
+  //   var value = obj[keyforobj];
+  //   //if value is obj
+  //   if (value instanceof Object) {
+  //     resultArrayForObj.push(stringifyJSON(value));
+  //   }
+  //   // if value is array
+  //   if (Array.isArray(value) {
+  //     resultArrayForObj.push(stringifyJSON(value));
+  //   })
+  //   if (typeof value === )
+  //   // if value is normal vlaue
+
+  //   return '{' + resultArrayForObj + '}'
+  // }
+  // return '[' + resultArrayForArray + ']'
+  //if its an obj, add an open bracket to the empty string
+  //and add a key value pair
+  //call stringify json again for the next item in the obj
+  //if you find an array, you add an open square bracket
+  //call stringify again (recurse) until you get to the end of the array, add a closing bracket
+  // at the end of the obj, get to the end of the closing bracket
+  //if you find a number or string, add it to the empty string
 };
